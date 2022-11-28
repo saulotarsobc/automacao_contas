d90e5570e8ddeb80a76da6d35ddfebca480c14e7
Saulo Costa <saulotarsobc@gmail.com>
2022-11-27 22:51:44 -0300

    :fire:

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from pyautogui import write, typewrite
 from time import sleep
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static')
 CORS(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 
 @app.route("/typewrite")
@@ -16,4 +21,4 @@ def hello_world():
     }
 
 
-app.run(host="192.168.3.21", port=3000, debug=True,)
+app.run(host="192.168.3.21", port=3000, debug=True)
