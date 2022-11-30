7675251a1d6d7d80a3968cce56603352471bd70c
Saulo Costa <saulotarsobc@gmail.com>
2022-11-30 09:36:02 -0300

    :fire:

--- render.sh
+++ render.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-rm -r build/ dist/ automatic.spec;
-pyinstaller -F -c --name automatic main.py;
+# rm -r build/ dist/ automatic.spec;
+# pyinstaller -F -c --name automatic main.py;
+pyinstaller -F -w --name automatic main.py;
